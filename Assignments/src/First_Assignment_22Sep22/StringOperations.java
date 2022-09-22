69e13d20f25516b9da78d471fcd84e995714ae86
Aditya Kumar <integrationoflogic@gmail.com>
2022-09-23 05:19:51 +0530

    Added all functionality of string

--- Assignments/src/First_Assignment_22Sep22/StringOperations.java
+++ Assignments/src/First_Assignment_22Sep22/StringOperations.java
@@ -9,16 +9,78 @@ v.) Once the duplicates are removed check whether the length of string is equal 
     string is PANGRAM or NOT.*/
 package First_Assignment_22Sep22;
 
+import java.util.*;
+
 public class StringOperations {
 	public static void main(String[] args) {
 		String s = "Co3zy s4phinx wav0es qu23art jug3 of b2ad m1ilk";
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) < 'A' || s.charAt(i) > 'Z' && s.charAt(i) < 'a'
-					|| s.charAt(i) > 'z' && s.charAt(i) == ' ') {
-				s = s.substring(0, i) + s.substring(i + 1);
-				i--;
+
+		// Remove the numbers
+
+		s = s.replaceAll("[0-9]", "");
+		System.out.println(s);
+
+		// Converting in lower case
+
+		s = s.toLowerCase();
+		System.out.println(s);
+
+		// Sorting the string without using predefined methods
+
+		String sortedString = "";
+		char tempString[] = s.toCharArray();
+		int n = tempString.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (tempString[i] > tempString[j]) {
+					char temp = tempString[i];
+					tempString[i] = tempString[j];
+					tempString[j] = temp;
+				}
 			}
 		}
-		System.out.println(s);
+		sortedString = new String(tempString);
+		System.out.println("Original String : " + s);
+		System.out.println("Sorted String : " + sortedString);
+
+		// Remove Duplicates from the string
+
+		StringBuilder sb = new StringBuilder();
+		Set<Character> linkedHashSet = new LinkedHashSet<>();
+		for (int i = 0; i < s.length(); i++) {
+			linkedHashSet.add(s.charAt(i));
+		}
+
+		for (Character c : linkedHashSet) {
+			sb.append(c);
+		}
+		System.out.println(sb);
+
+		// Checking length of string is 26 or not??
+
+		int len = sb.length();
+		if (len == 26) {
+			System.out.println("Length of string is equal to 26");
+		} else {
+			System.out.println("Length of string is not equal to 26");
+		}
+
+		// Checking PANGRAM or Not?
+
+		boolean[] mark = new boolean[26];
+		int index = 0;
+		for (int i = 0; i < sb.length(); i++) {
+
+			if ('a' <= sb.charAt(i) && sb.charAt(i) <= 'z')
+				index = sb.charAt(i) - 'a';
+			mark[index] = true;
+		}
+		for (int i = 0; i <= 25; i++) {
+			if (!mark[i]) {
+				System.out.println("The given string is not PANGRAM");
+				return;
+			}
+		}
+		System.out.println("Pangram String😄👍");
 	}
 }
